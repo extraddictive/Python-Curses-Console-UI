c576433586aee15eb946b2c100bd51df010233e1
extraddictive <extraddictive@marauder.site>
2024-08-17 23:38:54 +0300

    Add files via upload
    Components are now easier to add

--- console_ui.py
+++ console_ui.py
@@ -1,8 +1,7 @@
 import curses.ascii
-import sys
 import os
 import curses
-import time
+import string
 
 from typing import TYPE_CHECKING
 
@@ -13,6 +12,7 @@ else:
     from typing import Any
     CursesWindowType = Any
 
+# For Win10 at least
 if os.name == "nt":
     KEY_BACKSPACE = 8
 else:
@@ -23,151 +23,159 @@ else:
 el = "\r"
 nl = "\n"
 
-def isIpPortChar(c: int):
-    return curses.ascii.isdigit(c) or c == ord(".") or c == ord(":")
-
-class checkBox():
+class ui_component():
     def __init__(self,
                  x: int,
                  y: int,
+                 width: int,
+                 heigth: int,
                  window: CursesWindowType,
-                 label: str = "",
-                 checkedColorPair: int = None,
-                 uncheckedColorPair: int = None) -> None:
+                 enabled: bool = True,
+                 activeColorPair: int = 0,
+                 defaultColorPair: int = 0,
+                 disabledColorPair: int = 0) -> None:
         self.x = x
         self.y = y
-        self.len = len(label) + 3
+        self.width = width
+        self.heigth = heigth
         self.win = window
-        self.label = label
-        self.checkedColorPair = checkedColorPair
-        self.uncheckedColorPair = uncheckedColorPair
-        self._checked = False
+        self._enabled = enabled
+        self.activeColorPair = activeColorPair
+        self.defaultColorPair = defaultColorPair
+        self.disabledColorPair = disabledColorPair
 
-    def _drawUnchecked(self):
+        self._active = False
+    
+    def __new__(cls, comp, *args, **kwargs):
+        if cls.__base__ == ui_component:
+            if not isinstance(comp, ui_component):
+                raise TypeError(f"First parameter for component has to be an instance of {ui_component}")
+            ui_component.__init__(cls, **dict(zip(ui_component.__init__.__code__.co_varnames[1:], comp.__dict__.values())))
+        return super().__new__(cls)
+    
+    def _draw_default(self):
+        pass
+
+    def _draw_active(self):
+        pass
+
+    def _draw_disabled(self):
+        pass
+
+    def draw(self):
+        if self._enabled:
+            if self._active:
+                self._draw_active()
+                return
+            self._draw_default()
+        else:
+            self._draw_disabled()
+    
+    def is_at_coordinates_xy(self,
+                          x: int,
+                          y: int) -> bool:
+        return self.y >= y >= self.y + self.heigth and self.x <= x <= self.x + self.width
+    
+    # For height == 1
+    def is_at_coordinates_x(self,
+                          x: int,
+                          y: int) -> bool:
+        return y == self.y and self.x <= x <= self.x + self.width
+    
+    # For width == 1
+    def is_at_coordinates_y(self,
+                          x: int,
+                          y: int) -> bool:
+        return self.y >= y >= self.y + self.heigth and x == self.x
+    
+    def click(self,
+              x: int,
+              y: int):
+        if self.is_at_coordinates_x(x, y):
+            self._active = True
+            return
+        self._active = False
+        
+class checkBox(ui_component):
+    def __init__(self,
+                 comp: ui_component,
+                 label: str = "",) -> None:
+        self.label = label
+        self.width = len(self.label) + 3
+
+    def _draw_default(self):
 
             drawStr = self.label + "[ ]"
             if len(drawStr) == 0 and self.label:
                 drawStr = self.label
 
-            if len(drawStr) < self.len:
-                drawStr += "_" * (self.len - len(drawStr))
-
-            if self.uncheckedColorPair:
-                self.win.addstr(self.y, self.x, drawStr, curses.color_pair(self.uncheckedColorPair))
+            if self.defaultColorPair:
+                self.win.addstr(self.y, self.x, drawStr, curses.color_pair(self.defaultColorPair))
             else:
                 self.win.addstr(self.y, self.x, drawStr)
 
-    def _drawChecked(self):
-
+    def _draw_active(self):
             drawStr = self.label + "[X]"
-            if len(drawStr) < self.len:
-                drawStr += "<"
-                drawStr += "_" * (self.len - len(drawStr))
 
-            if self.checkedColorPair:
-                self.win.addstr(self.y, self.x, drawStr, curses.color_pair(self.checkedColorPair))
+            if self.activeColorPair:
+                self.win.addstr(self.y, self.x, drawStr, curses.color_pair(self.activeColorPair))
             else:
                 self.win.addstr(self.y, self.x, drawStr)
-
-    def draw(self):
-        if self._checked:
-            self._drawChecked()
-        else:
-            self._drawUnchecked()
-
-    def check(self):
-        self._checked = True
-
-    def uncheck(self):
-        self._checked = False
 
     def click(self,
               x: int,
               y: int):
-        if y == self.y and self.x <= x <= self.x + self.len:
-            if self._checked:
-                self.uncheck()
+        if self.is_at_coordinates_x(x, y):
+            if self._active:
+                self._active = False
                 return
-            self.check()
+            self._active = True
 
-class stringField():
+class stringField(ui_component):
     def __init__(self,
-                 x: int,
-                 y: int,
-                 length: int,
-                 window: CursesWindowType,
+                 comp: ui_component,
                  default: str = None,
-                 selColorPair: int = None,
-                 uslColorPair: int = None,
-                 inputFilter: callable = isIpPortChar) -> None:
-        self.x = x
-        self.y = y
-        self.len = length
-        self.win = window
+                 inputFilter: callable = curses.ascii.isalpha) -> None:
         self.default = default
-        self.selColorPair = selColorPair
-        self.uslColorPair = uslColorPair
         self.string = ""
         self.filter = inputFilter
-        self._selected = False
 
-    def _drawUnselected(self):
-
+    def _draw_default(self):
             drawStr = self.string
             if len(drawStr) == 0 and self.default:
                 drawStr = self.default
 
-            if len(drawStr) < self.len:
-                drawStr += "_" * (self.len - len(drawStr))
+            if len(drawStr) < self.width:
+                drawStr += "_" * (self.width - len(drawStr))
 
-            if self.uslColorPair:
-                self.win.addstr(self.y, self.x, drawStr, curses.color_pair(self.uslColorPair))
+            if self.defaultColorPair:
+                self.win.addstr(self.y, self.x, drawStr, curses.color_pair(self.defaultColorPair))
             else:
                 self.win.addstr(self.y, self.x, drawStr)
 
-    def _drawSelected(self):
-
+    def _draw_active(self):
             drawStr = self.string
-            if len(drawStr) < self.len:
+            if len(drawStr) < self.width:
                 drawStr += "<"
-                drawStr += "_" * (self.len - len(drawStr))
+                drawStr += "_" * (self.width - len(drawStr))
 
-            if self.selColorPair:
-                self.win.addstr(self.y, self.x, drawStr, curses.color_pair(self.selColorPair))
+            if self.activeColorPair:
+                self.win.addstr(self.y, self.x, drawStr, curses.color_pair(self.activeColorPair))
             else:
                 self.win.addstr(self.y, self.x, drawStr)
-
-    def draw(self):
-        if self._selected:
-            self._drawSelected()
-        else:
-            self._drawUnselected()
-
-    def select(self):
-        self._selected = True
-
-    def unselect(self):
-        self._selected = False
 
     def writech(self,
                 c: int):
-        if self._selected:
-            if len(self.string) < self.len and self.filter(c):
+        if self._active:
+            if len(self.string) < self.width and (not self.filter or self.filter(c)):
                 self.string += chr(c)
 
             if c == KEY_BACKSPACE and len(self.string) > 0:
                 self.string = self.string[:-1]
 
             if c == 10:
-                self.unselect()
+                self._active = False
 
-    def click(self,
-              x: int,
-              y: int):
-        if y == self.y and self.x <= x <= self.x + self.len:
-            self.select()
-            return
-        self.unselect()
 
 def console(screen: CursesWindowType):
     curses.start_color()
@@ -175,8 +183,8 @@ def console(screen: CursesWindowType):
     curses.init_pair( 3, curses.COLOR_YELLOW, curses.COLOR_WHITE)
 
     wInfo = curses.newwin(6, curses.COLS, 0, 0)
-    sInfo = stringField(1, 0, 20, wInfo, "ip:port", 1)
-    c_sus = checkBox(1, 3, wInfo, "Impostor", 1)
+    sInfo = stringField(ui_component(1, 0, 20, 1, wInfo, activeColorPair = 1), "Sus")
+    c_sus = checkBox(ui_component(1, 3, 20, 1, wInfo, activeColorPair = 1), "Impostor")
     #curses.noecho()
     curses.curs_set(0)
     curses.cbreak()
